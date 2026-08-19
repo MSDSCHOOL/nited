1b2ac76e41888fe2baf4ea4274b62673260bb423
Nureeda Asae <Nureeda078@gmail.com>
2026-08-19 15:38:43 +0700

    Update Apps Script URL to working deployment

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 // Configuration
 const CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzsjXNduH-R_GI0NKxceO4GaPwWAhRcObQKQQzyY-WCVZw8aDSDzgBl2krJEGet3MnEdQ/exec', // Replace with actual URL
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxFuboW0fxlyaHLP2YP_GdG_vN3OmPXF58IOXyN3DG92We_0oDToUJhNvN2h9LU2XXjlw/exec',
   SPREADSHEET_ID: '1HvvYyCkU2aPcdmo5zOAgUWzjBA0XXTISj64JmJramoc',
   DRIVE_FOLDER_ID: '1tbGMKsyUFQ16YjGCzNQbGRI099lCEH6k'
 };
